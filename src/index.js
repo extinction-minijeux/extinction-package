4e6a74b41c62956ac7196d2ef97f01ebbcf8428c
Lucas <741798+lucastrcts@users.noreply.github.com>
2020-05-03 17:11:58 +0200

    Compilation MacOS

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ if(process.platform  == 'win32'){
 } else if (process.platform == 'linux') {
   ppapi_flash_path = path.join(__dirname, 'libpepflashplayer.so');
 } else if (process.platform == 'darwin') {
-  ppapi_flash_path = path.join(__dirname, 'PepperFlashPlayer.plugin');
+  ppapi_flash_path = app.getPath('pepperFlashSystemPlugin');
 }
 
 app.commandLine.appendSwitch('ppapi-flash-path', ppapi_flash_path);
