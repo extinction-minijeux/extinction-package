d11256a4d26fcac7168059f70d5c95334e1fec91
Cyboulette <cyboulette58@gmail.com>
2020-05-03 13:05:28 +0200

    Initial commit with sources and readme

---
+++ readme.md
@@ -0,0 +1,31 @@
+## Extinction MiniJeux - Package
+
+**Compilation**  
+*Le projet utilise [electron](https://www.electronjs.org/) et [electron-forge](https://www.electronforge.io/) ainsi que le plugin [PepperFlashPlugin](https://www.electronjs.org/docs/tutorial/using-pepper-flash-plugin)*   
+
+Le code source principal se trouve dans `src/index.js` et plus exactement le bout de code suivant : `mainWindow.loadURL("https://www.extinction-minijeux.fr/executable.html");`  
+Electron nous permet alors d'embarquer un "mini-navigateur" avec le plugin flash et de charger la version navigateur sur un installateur pour Windows et Mac (ce qui explique donc qu'il fasse environ 70Mo)
+
+Pour compiler, il suffit de cloner le repo, faire un `npm install` puis `npm start` pour démarrer en temps réel ou `npm make` pour compiler (cela compilera en fonction du `package.json` et de votre machine).
+
+**Pourquoi Electron ?**  
+Suite à des problèmes de fluidité rencontrés avec Adobe Flash Player lorsque nous compilons depuis Adobe Animate 2020 ou Adobe Flash CS6, seule la version sur navigateur semble être fluide et corriger tous les problèmes. Electron nous permet alors de corriger ces problèmes.
+
+**Pourquoi Sourceforge et Github ?**  
+Nous n'avons pas les moyens de faire signer notre code, surtout pour la version Flash qui vise à disparaître, notre nom de domaine n'est pas reconnu comme "de confiance" pour les téléchargements car très peu de téléchargements ont été effectués. La publication sur Sourceforge et sur Github vous prouve qu'il n'y a aucun virus ou code malveillant dans ce que nous vous fournissons.
+
+**Contact**  
+Par mail : contact@extinction-minijeux.fr
+
+**License**  
+GNU GPL V3.0 - Disponible sur notre [github](https://github.com/extinction-minijeux/extinction-package/blob/master/LICENSE)
+
+## Extinction MiniJeux - Release notes
+**V1.0.0 - 03/05/2020** (même version que précédent car non déployée encore, les futures versions seront incrémentales)  
+- Ajout de l'option Zoom qui permet de retrouver le `100%` et `Afficher tout` de Flash
+- Publication du code source et création du projet sur Sourceforge
+
+**V1.0.0 - 02/05/2020**  
+- Création de l'installateur pour corriger les problèmes de fluidité, de mises à jour et d'antivirus.  
+- Création de la page Sourceforge qui assure une sécurité du code via l'analyse de Bitdefender  
+- L'installateur est plus lourd que l'ancienne version .exe car il embarque le système "Electron" qui est un "pseudo-navigateur" intégré au sein d'une application logicielle, cela nous permet de corriger les problèmes de fluidité rencontrés par nos joueurs.
---
+++ src/index.js
@@ -0,0 +1,95 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  return app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    'width': 820,
+    'height': 660,
+    'webPreferences': {
+      'plugins': true,
+      'nodeIntegration': true
+    },
+    'frame': true,
+    'icon': path.join(__dirname, 'icon.png')
+  });
+  mainWindow.setMenuBarVisibility(false);
+  // and load the URL of the app
+  mainWindow.loadURL("https://www.extinction-minijeux.fr/executable.html");
+  mainWindow.webContents.on('did-finish-load', () => {
+    setMainMenu(mainWindow); // Load the menu with mainWindow
+  });
+};
+
+function setMainMenu(mainWindow) {
+  const template = [
+    {
+      label: 'Zoom',
+      submenu: [
+        {
+          type: 'radio',
+          label: '100%',
+          checked: false,
+          click() {
+            let code = `document.getElementById('jeu').classList.add('full')`;
+            mainWindow.webContents.executeJavaScript(code);
+          }
+        },
+        {
+          type: 'radio',
+          checked: true,
+          label: 'Afficher tout',
+          click() {
+            let code = `document.getElementById('jeu').classList.remove('full')`;
+            mainWindow.webContents.executeJavaScript(code);
+          }
+        }
+      ]
+    }
+  ];
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+  mainWindow.setMenuBarVisibility(true);
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+// load flash
+let ppapi_flash_path;
+if(process.platform  == 'win32'){
+  ppapi_flash_path = path.join(__dirname, 'pepflashplayer.dll');
+} else if (process.platform == 'linux') {
+  ppapi_flash_path = path.join(__dirname, 'libpepflashplayer.so');
+} else if (process.platform == 'darwin') {
+  ppapi_flash_path = path.join(__dirname, 'PepperFlashPlayer.plugin');
+}
+
+app.commandLine.appendSwitch('ppapi-flash-path', ppapi_flash_path);
+app.commandLine.appendSwitch('ppapi-flash-version', '32.0.0.363');
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
